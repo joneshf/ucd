cf0e854c76790694ad2b92b45f1ea92fdfb04020
joneshf <jones3.hardy@gmail.com>
2013-10-14 13:21:13 -0700

    Fixed recursive step and added type signature.

--- ecs154A/GrayCode.hs
+++ ecs154A/GrayCode.hs
@@ -1,9 +1,9 @@
 module GrayCode where
 
---gray :: Int -> [[]]
+gray :: Int -> [String]
 gray 0 = []
 gray 1 = ["0", "1"]
 gray n = map ("0"++) old ++ map ("1"++) new
   where
     old = gray (n-1)
-    new = map (\b -> if b == "0" then "1" else b) old
+    new = map (map (\b -> if b == '0' then '1' else '0')) old
