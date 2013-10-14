d5fb656e563b37f263182804f142847f9964e42e
joneshf <jones3.hardy@gmail.com>
2013-10-14 14:01:32 -0700

    Gray code in proper oder, and added binary.

--- ecs154A/GrayCode.hs
+++ ecs154A/GrayCode.hs
@@ -3,7 +3,13 @@ module GrayCode where
 gray :: Int -> [String]
 gray 0 = []
 gray 1 = ["0", "1"]
-gray n = map ("0"++) old ++ map ("1"++) new
+gray n = map ('0':) old ++ (map ('1':) . reverse) old
   where
     old = gray (n-1)
-    new = map (map (\b -> if b == '0' then '1' else '0')) old
+
+binary :: Int -> [String]
+binary 0 = []
+binary 1 = ["0", "1"]
+binary n = map ('0':) old ++ map ('1':) old
+  where
+    old = binary (n-1)
