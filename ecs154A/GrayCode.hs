7132909701976e8fecc7b42b2be33da68b88fae7
joneshf <jones3.hardy@gmail.com>
2013-10-18 13:24:44 -0700

    Actually handle the base case properly.

--- ecs154A/GrayCode.hs
+++ ecs154A/GrayCode.hs
@@ -1,15 +1,13 @@
 module GrayCode where
 
 gray :: Int -> [String]
-gray 0 = []
-gray 1 = ["0", "1"]
+gray 0 = [""]
 gray n = map ('0':) old ++ (map ('1':) . reverse) old
   where
     old = gray (n-1)
 
 binary :: Int -> [String]
-binary 0 = []
-binary 1 = ["0", "1"]
+binary 0 = [""]
 binary n = map ('0':) old ++ map ('1':) old
   where
     old = binary (n-1)
