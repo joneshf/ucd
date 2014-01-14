295a55aa1fb9e67434fb6ed020e360833d293c3b
joneshf <jones3.hardy@gmail.com>
2014-01-14 01:09:33 -0800

    1 and 2.

---
+++ ecs140a/room.pl
@@ -0,0 +1,6 @@
+room --> (table; chairs, table; table, [couch]; table, stuff).
+table --> ([oak]; [teak]; set).
+set --> (table, chairs; [dinette]).
+chairs --> (chair, chairs; chair).
+chair --> ([armchair]; [bench]; [stool]).
+stuff --> ([rug]; [plant]).
