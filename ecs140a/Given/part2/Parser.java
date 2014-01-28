07ced61a75589ffd381145d489a8bc8ef55ea528
joneshf <jones3.hardy@gmail.com>
2014-01-27 17:58:31 -0800

    Alphabetize.

--- ecs140a/Given/part2/Parser.java
+++ ecs140a/Given/part2/Parser.java
@@ -48,6 +48,28 @@ public class Parser {
     }
 
     /**
+     * Parses declarations.
+     * Declarations are `var {id} rav`
+     *
+     * @param scanner The scanner we're reading tokens from.
+     */
+    private Validation<String, Token> parseDeclaration(Scan scanner) {
+        // Snag the next token.
+        Token next = scanner.scan();
+        // We can have zero or more `id`'s
+        while (next.kind == TK.ID) {
+            next = scanner.scan();
+        }
+
+        // The token wasn't an id, it better be a `rav`.
+        if (next.kind == TK.RAV) {
+            return Validation.valid(next);
+        } else {
+            return Validation.invalid("Expected rav");
+        }
+    }
+
+    /**
      * Parses expressions.
      * Expressions are `simple` followed by zero or more `relop simple`.
      *
@@ -62,99 +84,6 @@ public class Parser {
             return parsed;
         } else {
             return manyExpression(scanner);
-        }
-    }
-
-    /**
-     * Parses zero or more `expression`'s
-     *
-     * @param scanner The scanner we're reading tokens from.
-     */
-    private Validation<String, Token> manyExpression(Scan scanner) {
-        // We can have zero or more `relop simple`'s
-        Token next = scanner.scan();
-        System.out.println("Parsing manyExpression.");
-        System.out.println(next.toString());
-        switch (next.kind) {
-            case PLUS:
-            case MINUS:
-                return parseExpression(scanner);
-            default:
-                return parse(scanner, next);
-        }
-    }
-
-    /**
-     * Parses simples.
-     * Simples are `term`'s followed by zero or more `addop term`'s
-     *
-     * @param scanner The scanner we're reading tokens from.
-     */
-    private Validation<String, Token> parseSimple(Scan scanner) {
-        // We need the first thing to be a term.
-        Validation<String, Token> parsed = parseTerm(scanner);
-        if (parsed.isInvalid()) {
-            return parsed;
-        } else if (((Token) parsed.value()).kind == TK.EOF) {
-            return parsed;
-        } else {
-            return manySimple(scanner);
-        }
-    }
-
-    /**
-     * Parses zero or more `simple`'s
-     *
-     * @param scanner The scanner we're reading tokens from.
-     */
-    private Validation<String, Token> manySimple(Scan scanner) {
-        // We can have zero or more `addop term`'s
-        Token next = scanner.scan();
-        switch (next.kind) {
-            case EQ:
-            case NE:
-            case LT:
-            case GT:
-            case LE:
-            case GE:
-                return parseSimple(scanner);
-            default:
-                return parse(scanner, next);
-        }
-    }
-
-    /**
-     * Parses terms.
-     * Terms are `factor`'s followed by zero or more `multop factor`'s
-     *
-     * @param scanner The scanner we're reading tokens from.
-     */
-    private Validation<String, Token> parseTerm(Scan scanner) {
-        // // We need the first thing to be a factor.
-        Validation<String, Token> parsed = parseFactor(scanner);
-        if (parsed.isInvalid()) {
-            return parsed;
-        } else if (((Token) parsed.value()).kind == TK.EOF) {
-            return parsed;
-        } else {
-            return manyTerm(scanner);
-        }
-    }
-
-    /**
-     * Parses zero or more `term`'s
-     *
-     * @param scanner The scanner we're reading tokens from.
-     */
-    private Validation<String, Token> manyTerm(Scan scanner) {
-        // We can have zero or more `multop factor`'s
-        Token next = scanner.scan();
-        switch (next.kind) {
-            case TIMES:
-            case DIVIDE:
-                return parseTerm(scanner);
-            default:
-                return parse(scanner, next);
         }
     }
 
@@ -184,24 +113,95 @@ public class Parser {
     }
 
     /**
-     * Parses declarations.
-     * Declarations are `var {id} rav`
+     * Parses simples.
+     * Simples are `term`'s followed by zero or more `addop term`'s
      *
      * @param scanner The scanner we're reading tokens from.
      */
-    private Validation<String, Token> parseDeclaration(Scan scanner) {
-        // Snag the next token.
-        Token next = scanner.scan();
-        // We can have zero or more `id`'s
-        while (next.kind == TK.ID) {
-            next = scanner.scan();
-        }
-
-        // The token wasn't an id, it better be a `rav`.
-        if (next.kind == TK.RAV) {
-            return Validation.valid(next);
+    private Validation<String, Token> parseSimple(Scan scanner) {
+        // We need the first thing to be a term.
+        Validation<String, Token> parsed = parseTerm(scanner);
+        if (parsed.isInvalid()) {
+            return parsed;
+        } else if (((Token) parsed.value()).kind == TK.EOF) {
+            return parsed;
         } else {
-            return Validation.invalid("Expected rav");
+            return manySimple(scanner);
+        }
+    }
+
+    /**
+     * Parses terms.
+     * Terms are `factor`'s followed by zero or more `multop factor`'s
+     *
+     * @param scanner The scanner we're reading tokens from.
+     */
+    private Validation<String, Token> parseTerm(Scan scanner) {
+        // // We need the first thing to be a factor.
+        Validation<String, Token> parsed = parseFactor(scanner);
+        if (parsed.isInvalid()) {
+            return parsed;
+        } else if (((Token) parsed.value()).kind == TK.EOF) {
+            return parsed;
+        } else {
+            return manyTerm(scanner);
+        }
+    }
+
+    /**
+     * Parses zero or more `expression`'s
+     *
+     * @param scanner The scanner we're reading tokens from.
+     */
+    private Validation<String, Token> manyExpression(Scan scanner) {
+        // We can have zero or more `relop simple`'s
+        Token next = scanner.scan();
+        System.out.println("Parsing manyExpression.");
+        System.out.println(next.toString());
+        switch (next.kind) {
+            case PLUS:
+            case MINUS:
+                return parseExpression(scanner);
+            default:
+                return parse(scanner, next);
+        }
+    }
+
+    /**
+     * Parses zero or more `simple`'s
+     *
+     * @param scanner The scanner we're reading tokens from.
+     */
+    private Validation<String, Token> manySimple(Scan scanner) {
+        // We can have zero or more `addop term`'s
+        Token next = scanner.scan();
+        switch (next.kind) {
+            case EQ:
+            case NE:
+            case LT:
+            case GT:
+            case LE:
+            case GE:
+                return parseSimple(scanner);
+            default:
+                return parse(scanner, next);
+        }
+    }
+
+    /**
+     * Parses zero or more `term`'s
+     *
+     * @param scanner The scanner we're reading tokens from.
+     */
+    private Validation<String, Token> manyTerm(Scan scanner) {
+        // We can have zero or more `multop factor`'s
+        Token next = scanner.scan();
+        switch (next.kind) {
+            case TIMES:
+            case DIVIDE:
+                return parseTerm(scanner);
+            default:
+                return parse(scanner, next);
         }
     }
 }
