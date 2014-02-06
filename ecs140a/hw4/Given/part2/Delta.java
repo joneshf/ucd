4cfc24dbc4cdb918ebdcb683bb11b7e6e8dba7f2
joneshf <jones3.hardy@gmail.com>
2014-02-06 14:30:43 -0800

    Done with part2.

---
+++ ecs140a/hw4/Given/part2/Delta.java
@@ -0,0 +1,19 @@
+public class Delta extends Seq {
+    protected int num;
+    protected int initial;
+    protected int delta;
+
+    public Delta(int num, int initial, int delta) {
+        this.num = num;
+        this.initial = num == 0 ? 0 : initial;
+        this.delta = num == 0 ? 0 : delta;
+    }
+
+    public String toString() {
+        return "< "+this.num+" : "+this.initial+" &"+this.delta+" >";
+    }
+
+    public int min() {
+        return Math.min(this.initial, this.initial + this.delta * (this.num - 1));
+    }
+}
