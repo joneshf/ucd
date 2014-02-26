f7a5eb7a6d96bc119e759b907c40a2977cc090b8
joneshf <jones3.hardy@gmail.com>
2014-02-25 19:43:36 -0800

    Lots of changes, most importantly, use the GSM for more things.

--- ecs170/ForDistribution/minimax_joneshf.java
+++ ecs170/ForDistribution/minimax_joneshf.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class minimax_joneshf extends AIModule {
 
-    public int[][] board = new int[7][6];
+    public int[][] board;
 
     public int player;
     public int opponent;
@@ -16,8 +16,8 @@ public class minimax_joneshf extends AIModule {
         this.opponent = 0;
     }
 
-    public void showBoard(int[][] board) {
-        System.out.println(Arrays.deepToString(board));
+    public void showBoard(final GameStateModule game) {
+        this.td.drawBoard(game);
     }
 
     public void getNextMove(final GameStateModule game) {
@@ -27,6 +27,7 @@ public class minimax_joneshf extends AIModule {
         this.opponent = player == 1 ? 2 : 1;
         this.width = game.getWidth();
         this.height = game.getHeight();
+        this.board = new int[this.width][this.height];
         // Enumerate the board.
         for (int i = 0; i < game.getWidth(); ++i) {
             for (int j = 0; j < game.getHeight(); ++j) {
@@ -34,106 +35,126 @@ public class minimax_joneshf extends AIModule {
             }
         }
         System.out.println("that took: "+((System.nanoTime() - start)));
-        showBoard(this.board);
-        this.td.drawBoard(game);
+        showBoard(game);
         int depth = 0;
-        while (!this.terminate) {
-            minimaxDecision(depth++, this.board, game);
+        while (!this.terminate && depth < 3) {
+            minimaxDecision(depth++, game);
         }
     }
 
-    public void minimaxDecision(int depth, int[][] state, final GameStateModule game) {
-        int[] v = maxValue(depth, state, game);
+    public void minimaxDecision(int depth, final GameStateModule game) {
+        int[] v = maxValue(depth, this.width / 2, game);
+        System.out.println("Choosing "+v[1]);
         this.chosenMove = v[1];
     }
 
-    public int[] maxValue(int depth, int[][] state, final GameStateModule game) {
+    public int[] maxValue(int depth, int col, final GameStateModule game) {
 
-        if (depth == 0 || this.terminate || terminal(state)) {
-            return utility(state);
+        System.out.println("In maxValue");
+        showBoard(game);
+        if (depth == 0 || this.terminate || game.isGameOver()) {
+            return utility(col, game);
         }
-
-        System.out.println("in maxValue");
-        showBoard(state);
 
         int[] v = {-Integer.MAX_VALUE, 0};
         int temp[];
-        for (int[][] s : getMoves(this.player, state, game)) {
-            temp = minValue(depth - 1, s, game);
+        for (int c : getMoves(this.player, game)) {
+            game.makeMove(c);
+            temp = minValue(depth - 1, c, game);
             v = v[0] < temp[0] ? temp : v;
+            game.unMakeMove();
         }
 
         return v;
 
     }
 
-    public int[] minValue(int depth, int[][] state, final GameStateModule game) {
+    public int[] minValue(int depth, int col, final GameStateModule game) {
 
-        if (depth == 0 || this.terminate || terminal(state)) {
-            return utility(state);
+        System.out.println("In minValue");
+        showBoard(game);
+        if (depth == 0 || this.terminate || game.isGameOver()) {
+            return utility(col, game);
         }
-
-        System.out.println("in minValue");
-        showBoard(state);
 
         int[] v = {Integer.MAX_VALUE, 0};
         int temp[];
-        for (int[][] s : getMoves(this.opponent, state, game)) {
-            temp = maxValue(depth - 1, s, game);
+        for (int c : getMoves(this.opponent, game)) {
+            game.makeMove(c);
+            temp = maxValue(depth - 1, c, game);
             v = v[0] > temp[0] ? temp : v;
+            game.unMakeMove();
         }
 
         return v;
 
     }
 
-    public boolean terminal(int[][] state) {
+    public boolean terminal(final GameStateModule game) {
+        long[] boards = gameToBits(game);
+
+        return checkWins(boards[0]) || checkWins(boards[1]);
+    }
+
+    public long[] gameToBits(final GameStateModule game) {
         long playerBoard = 0x0000000000000000l;
         long opponentBoard = 0x0000000000000000l;
         int pos = 0;
         for (int c = 0; c < this.width; ++c) {
             for (int r = 0; r < this.height; ++r) {
-                if (state[c][r] == this.player) {
-                    playerBoard |= (c+1) << r;
-                } else if (state[c][r] == this.opponent) {
-                    opponentBoard |= (c+1) << r;
+                if (game.getAt(c, r) == this.player) {
+                    playerBoard |= 1 << (c * this.height + r);
+                } else if (game.getAt(c, r) == this.opponent) {
+                    opponentBoard |= 1 << (c * this.height + r);
                 }
             }
         }
 
-        System.out.println(playerBoard);
-        System.out.println(opponentBoard);
+        return new long[] {playerBoard, opponentBoard};
+    }
+
+    public boolean checkWins(long board) {
+        long temp = board & (board >> (this.width - 1));
+        if (0 < (temp & (temp >> 2 * (this.width - 1)))) {
+            return true;
+        }
+        temp = board & (board >> this.width);
+        if (0 < (temp & (temp >> 2 * this.width))) {
+            return true;
+        }
+        temp = board & (board >> (this.width + 1));
+        if (0 < (temp & (temp >> 2 * (this.width + 1)))) {
+            return true;
+        }
+        temp = board & (board >> 1);
+        if (0 < (temp & (temp >> 2 * 1))) {
+            return true;
+        }
 
         return false;
     }
 
-    public int[] utility(int[][] state) {
-        int[] util = {0, 0};
-        return util;
+    public int[] utility(int col, final GameStateModule game) {
+        long[] boards = gameToBits(game);
+        if (checkWins(boards[0])) {
+            return new int[] {100, col};
+        } else if (checkWins(boards[1])) {
+            return new int[] {-100, col};
+        } else {
+            return new int[] {0, col};
+        }
     }
 
-    public int[][][] getMoves(int player, int[][] board, final GameStateModule game) {
-        List<int[][]> moves = new ArrayList<int[][]>();
-        int height;
+    public List<Integer> getMoves(int player, final GameStateModule game) {
+        List<Integer> moves = new ArrayList<Integer>();
+        int row;
         int size = 0;
-        for (int i = 0; i < board.length; ++i) {
-            if (game.canMakeMove(i)) {
+        for (int col = 0; col < this.width; ++col) {
+            if (game.canMakeMove(col)) {
                 ++size;
-                int[][] newBoard = deepCopy(board);
-                height = game.getHeightAt(i);
-                newBoard[i][height] = player;
-                moves.add(newBoard);
+                moves.add(col);
             }
         }
-        return moves.toArray(new int[size][][]);
-    }
-
-    public int[][] deepCopy(int[][] src) {
-        int[][] dest = new int[src.length][];
-        for (int i = 0; i < src.length; ++i) {
-            dest[i] = Arrays.copyOf(src[i], src[i].length);
-        }
-
-        return dest;
+        return moves;
     }
 }
