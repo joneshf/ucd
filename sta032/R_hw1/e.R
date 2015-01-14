bc662d346a73c43c5a36b5adbb67ba05447151c3
joneshf <jones3.hardy@gmail.com>
2015-01-14 00:50:25 -0800

    Update paths.

--- sta032/R_hw1/e.R
+++ sta032/R_hw1/e.R
@@ -1,4 +1,4 @@
-> png(filename="R_hw1/e.png")
+> png(filename="e.png")
 > hist(data$GPA, main="GPA for Intro. Stats. at Wash. Uni.", xlab="GPA",
       ylab="# of Students")
 > dev.off()
--- sta032/R_hw1/f.R
+++ sta032/R_hw1/f.R
@@ -1,6 +1,6 @@
 > fallGPA <- subset(data, Semester=="Fall", select=GPA)$GPA
 > springGPA <- subset(data, Semester=="Spring", select=GPA)$GPA
-> png(filename="R_hw1/f.png")
+> png(filename="f.png")
 > boxplot(fallGPA, springGPA, main="GPA for Intro. Stats. at Wash. Uni.",
       names=c("Fall", "Spring"), ylab="GPA")
 > dev.off()
