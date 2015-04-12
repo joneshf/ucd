a104dc07d0923e999f4220209603af9823e4f929
joneshf <jones3.hardy@gmail.com>
2015-04-12 09:48:42 -0700

    Started trying to fix things.

--- mat168/p1.hs
+++ mat168/p1.hs
@@ -1,50 +1,69 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Werror #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
 module P1 where
 
-import Control.Applicative ((<$>), (<*>), (<*))
+import Control.Lens
 
-import Data.Char (isUpper)
-import Data.Foldable (foldl')
+import Data.Char (digitToInt,isUpper)
 
-import Text.Parsec (anyChar, char, choice, digit, endOfLine, many1, manyTill, satisfy, spaces, string, try)
+import Text.Parsec (anyChar, chainl1, char, choice, digit, endOfLine, manyTill, satisfy, spaces, string, try)
 import Text.Parsec.String (Parser, parseFromFile)
 
-data Node = Node
-    { number :: Int
-    , x      :: Float
-    , y      :: Float
+newtype Degree = Degree { _runDegree :: Int }
+    deriving (Enum, Eq, Integral, Num, Ord, Real, Show)
+newtype Minute = Minute { _runMinute :: Int }
+    deriving (Enum, Eq, Integral, Num, Ord, Real, Show)
+
+data Geo = Geo
+    { _degree :: Degree
+    , _minute :: Minute
     } deriving (Eq, Show)
+
+data Node = Node
+    { _number    :: Int
+    , _latitude  :: Geo
+    , _longitude :: Geo
+    } deriving (Eq, Show)
+
+makeLenses ''Node
+makeLenses ''Geo
+makeLenses ''Degree
+makeLenses ''Minute
 
 parseNodes :: Parser [Node]
 parseNodes = do
-    manyTill anyChar (try (string "NODE_COORD_SECTION"))
-    manyTill parseNode (try (satisfy isUpper))
+    manyTill anyChar $ try $ string "NODE_COORD_SECTION"
+    manyTill parseNode $ try $ satisfy isUpper
 
 parseNode :: Parser Node
 parseNode =
-    Node <$> (spaces >> read <$> many1 digit)
-         <*> parseFloat
-         <*> parseFloat
+    Node <$> (spaces *> parseInt)
+         <*> parseGeo
+         <*> parseGeo
          <*  endOfLine
 
-parseFloat :: Parser Float
-parseFloat = spaces >> choice [pos, neg, float]
+parseGeo :: Parser Geo
+parseGeo = spaces *> choice [pos, neg, geo]
     where
-    pos = char '+' >> float
-    neg = char '-' >> negate <$> float
-    float = do
-        characteristic <- many1 digit
-        char '.'
-        mantissa <- many1 digit
-        return $ read $ characteristic ++ "." ++ mantissa
+    pos = char '+' *> geo
+    neg = char '-' *> geo
+    geo =
+        Geo <$> (Degree <$> parseInt) <* char '.'
+            <*> (Minute <$> parseInt)
+
+parseInt :: Parser Int
+parseInt = chainl1 (digitToInt <$> digit) (pure $ (+) . (* 10))
 
 distance :: Node -> Node -> Float
-distance a b = sqrt $ xx * yy
-    where
-    xx = (x a - x b) * (x a - x b)
-    yy = (y a - y b) * (y a - y b)
+distance a b = sqrt $ fromIntegral ((((a^.latitude.degree - b^.latitude.degree)^(2 :: Int) * (a^.longitude.degree - b^.longitude.degree)^(2 :: Int)))^.runDegree)
+--distance _ _ = 1
+--distance a b = sqrt $ xx * yy
+--    where
+--    xx = (runGeo (latitude a) - runGeo (latitude b)) * (runGeo (latitude a) - runGeo (latitude b))
+--    yy = (runGeo (longitude a) - runGeo (longitude b)) * (runGeo (longitude a) - runGeo (longitude b))
 
 tour :: [Node] -> [(Node, Node)]
 tour xs = zip xs rot
@@ -53,7 +72,7 @@ tour xs = zip xs rot
     (h, t) = splitAt 1 xs
 
 tourDistance :: [(Node, Node)] -> Float
-tourDistance = foldl' (\acc (a, b) -> acc + distance a b) 0
+tourDistance = sum . fmap (uncurry distance)
 
 main :: IO ()
 main = do
