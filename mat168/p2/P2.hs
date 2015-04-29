ce25d6176c8300f53a1e7d4b2d237d231d99c56d
joneshf <jones3.hardy@gmail.com>
2015-04-29 04:24:26 -0700

    Move printing out.

--- mat168/p2/P2.hs
+++ mat168/p2/P2.hs
@@ -27,75 +27,17 @@ module P2 where
 
 -- We import a whole mess of helper things
 
-import Control.Lens ((^.), _1, _2, _3)
-
 import Data.Foldable (for_)
-import Data.Int (Int32)
 import Data.List (sort)
 
-import P2.Distance (Distance(..))
-import P2.Morphism -- (incPairsDists, tourDistance)
-import P2.Node (Node, Nodes(..))
 import P2.Parser (parseData)
+import P2.Printing (prettyDirect, prettyIncPairs, prettyNearest)
 
-import System.FilePath ((</>), replaceExtension, takeFileName)
 import System.FilePath.Glob (glob)
 
 import Text.Parsec.String (parseFromFile)
-import Text.Printf (PrintfType, printf)
 
 -- And off we go.
-
--- We provide some output formatting
-
-format :: PrintfType a => Int -> Int32 -> a
-format = printf
-    "The distance of the tour in order 1-2-...-%d-1 is: %d km\n"
-format' :: (Distance s, PrintfType a) => [Node s] -> a
-format' xs = format (length xs) . tourDistance $ xs
-
-cataNode :: PrintfType a
-         => (forall s. (Distance s) => ([Node s] -> a))
-         -> Nodes
-         -> a
-cataNode f (GEOS    ns) = f ns
-cataNode f (EUC_2DS ns) = f ns
-
-burma14 :: IO ()
-burma14 = do
-    putStrLn "Increasing tour of burma14.tsp"
-    d <- parseFromFile parseData "all_tsp/burma14.tsp"
-    case d of
-        Left e -> print e
-        Right (GEOS    ns) -> for_ (incPairsDists ns) $ \x ->
-            printf "%d %d %d\n" (x^._1) (x^._2) (x^._3)
-        Right (EUC_2DS ns) -> for_ (incPairsDists ns) $ \x ->
-            printf "%d %d %d\n" (x^._1) (x^._2) (x^._3)
-
-prettyDirect :: FilePath -> Nodes -> IO ()
-prettyDirect tsp n = do
-    printf "Tour of %s\n" (takeFileName tsp)
-    cataNode format' n
-
-prettyInc :: Distance s => [Node s] -> String
-prettyInc xs =
-    unlines . (len:) . fmap (uncurry3 $ printf "%d %d %d") . incPairsDists $ xs
-    where
-    len = show $ length xs
-    uncurry3 f (x, y, z) = f x y z
-
-writeIncPairs :: Distance s => FilePath -> [Node s] -> IO ()
-writeIncPairs fp = writeFile fp . prettyInc
-
-incPairsDir :: FilePath
-incPairsDir = "inc_pairs"
-
-incPairsExt :: FilePath
-incPairsExt = "inc"
-
-mkIncPairsName :: FilePath -> FilePath
-mkIncPairsName fp =
-    incPairsDir </> replaceExtension (takeFileName fp) incPairsExt
 
 main :: IO ()
 main = do
@@ -105,11 +47,8 @@ main = do
     tsps <- mapM (parseFromFile parseData) tspFiles
     let tsps' = [(fp, ns) | (fp, Right ns) <- zip tspFiles tsps]
     -- We first need to write the increasing distance pairs for each file.
-    -- for_ (take 1 tsps') $ \(fp, n) ->
-    --     cataNode (writeIncPairs $ mkIncPairsName fp) n
+    for_ tsps' $ uncurry prettyIncPairs
     -- Next we want to take compute the direct tour from 1 to n.
     for_ tsps' $ uncurry prettyDirect
     -- Now we need to do the nearest neighbor.
-    for_ tsps' $ \(fp, n) -> do
-        print fp
-        cataNode (print . nearestDistance) n
+    for_ tsps' $ uncurry prettyNearest
